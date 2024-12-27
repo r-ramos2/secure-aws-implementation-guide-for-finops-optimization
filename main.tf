0654755eec9039df7b384d50198e4be28da1d2f9
R <90172092+r-ramos2@users.noreply.github.com>
2024-12-26 22:47:43 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -109,11 +109,3 @@ resource "aws_cloudwatch_metric_alarm" "cost_alarm" {
     ServiceName = "AWS"
   }
 }
-
-output "s3_bucket_name" {
-  value = aws_s3_bucket.secure_bucket.bucket
-}
-
-output "sns_topic_arn" {
-  value = aws_sns_topic.cost_alerts.arn
-}
